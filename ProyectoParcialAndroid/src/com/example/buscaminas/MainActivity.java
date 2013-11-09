b8ec1185288075fea9edd600bd00cadcceb20fcc
Gabriel Falcones <gabofalc@hotmail.com>
2013-11-09 18:54:59 -0500

    Desarrollo del menu principal (botones Jugar,continuar,instrucciones,puntuaciones,salir)

--- ProyectoParcialAndroid/src/com/example/buscaminas/MainActivity.java
+++ ProyectoParcialAndroid/src/com/example/buscaminas/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.buscaminas;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +18,9 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	public void salir(View view){
+		finish();
 	}
 
 }
